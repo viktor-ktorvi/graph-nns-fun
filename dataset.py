94835a7f935bfe3933f5fd98c17cb224e53526ac
viktor-ltorvi <todosijevicviktor998@gmail.com>
2023-02-28 19:11:49 +0100

    set seeds; draw sample

--- dataset.py
+++ dataset.py
@@ -12,7 +12,7 @@ from typing import Union, List, Tuple
 from matplotlib import cm
 from matplotlib import pyplot as plt
 
-from gaussian_landscape import feature_target_plot, flip_y_axis, get_node_values, generate_graph, generate_statistics
+from gaussian_landscape import feature_target_plot, flip_y_axis, get_landscape_value, get_node_values, generate_graph, generate_statistics
 
 
 class GaussianLandscapeDataset(InMemoryDataset):
@@ -44,6 +44,7 @@ class GaussianLandscapeDataset(InMemoryDataset):
 
         self.generate_data(cfg.dataset.size)
 
+        self.task = cfg.dataset.task
         if cfg.dataset.task == "classification":
             self.classification_transform()
 
@@ -63,14 +64,64 @@ class GaussianLandscapeDataset(InMemoryDataset):
     def download(self):
         pass
 
-    def draw_graph(self, **kwargs):
-        nx.draw(self.graph, flip_y_axis(self.pos_dict), **kwargs)
-
     def draw_basis(self, **kwargs):
-        feature_target_plot(self.graph, self.pos,
+        feature_target_plot(self.graph, self.pos_dict,
                             self.feature_means, self.feature_covs,
                             self.target_means, self.target_covs,
                             **kwargs)
+
+    def draw_graph(self, **kwargs):
+        plt.title("Graph")
+        nx.draw(self.graph, flip_y_axis(self.pos_dict), **kwargs)
+
+    def draw_sample(self, idx, xlim=(-1, 1), ylim=(-1, 1), resolution=100, cmap_features=cm.viridis, cmap_targets=cm.coolwarm):
+        sample = self.get(idx)
+        num_features = sample.x.shape[1]
+
+        X, Y = np.meshgrid(np.linspace(xlim[0], xlim[1], resolution),
+                           np.linspace(ylim[0], ylim[1], resolution))
+
+        fig, ax = plt.subplots(1, num_features + 1, sharey="row")
+        fig.suptitle("Features and target of sample #{:d}".format(idx))
+
+        for i in range(num_features):
+            ax[i].set_title("# {:d}".format(i))
+
+            # feature maps
+            feature_landscape = get_landscape_value(X, Y, self.feature_means[i], self.feature_covs[i])
+
+            # graph with feature map values
+            nx.draw(
+                self.graph, flip_y_axis(self.pos_dict),
+                node_color=sample.x[:, i].cpu().numpy(),
+                cmap=cmap_features,
+                vmin=np.min(feature_landscape),
+                vmax=np.max(feature_landscape),
+                ax=ax[i]
+            )
+
+        # target
+        target = sample.y.cpu().numpy()
+
+        if self.task == "regression":
+            regression_landscape = get_landscape_value(X, Y, self.target_means, self.target_covs)
+            vmin = np.min(regression_landscape)
+            vmax = np.max(regression_landscape)
+        elif self.task == "classification":
+            vmin = np.min(target)
+            vmax = np.max(target)
+        else:
+            raise NotImplemented
+
+        ax[-1].set_title("target")
+        nx.draw(
+            self.graph, flip_y_axis(self.pos_dict),
+            node_color=target,
+            cmap=cmap_targets,
+            vmin=vmin,
+            vmax=vmax,
+            ax=ax[-1]
+        )
 
     def get(self, idx: int) -> Data:
         assert idx < self.len(), "Index(={:d}) is higher then the length of the dataset(={:d})".format(idx, self.len())
@@ -85,7 +136,7 @@ class GaussianLandscapeDataset(InMemoryDataset):
         :param high: float; high value of the uniform distribution
         :return:
         """
-        for i in tqdm(range(num), ascii=True, desc="Generating data"):
+        for _ in tqdm(range(num), ascii=True, desc="Generating data"):
             sample = copy.deepcopy(self.data_basis)
             sample.x *= torch.FloatTensor(*sample.x.shape).uniform_(low, high)
             sample.y *= torch.FloatTensor(*sample.y.shape).uniform_(low, high)
@@ -109,14 +160,34 @@ class GaussianLandscapeDataset(InMemoryDataset):
 
 @hydra.main(version_base=None, config_path="configs", config_name="default")
 def main(cfg):
+    np.random.seed(cfg.random_seed.numpy)
+    torch.random.manual_seed(cfg.random_seed.torch)
+
     dataset = GaussianLandscapeDataset(cfg)
 
     plt.figure()
     dataset.draw_graph()
 
-    dataset.draw_basis(cmap_features=cm.viridis, cmap_targets=cm.coolwarm,
-                       resolution=cfg.plotting.resolution,
-                       xlim=cfg.plotting.xlim, ylim=cfg.plotting.ylim)
+    dataset.draw_basis(
+        cmap_features=cm.viridis, cmap_targets=cm.coolwarm,
+        resolution=cfg.plotting.resolution,
+        xlim=cfg.plotting.xlim, ylim=cfg.plotting.ylim
+    )
+
+    dataset.draw_sample(
+        7,
+        cmap_features=cm.viridis, cmap_targets=cm.coolwarm,
+        resolution=cfg.plotting.resolution,
+        xlim=cfg.plotting.xlim, ylim=cfg.plotting.ylim
+    )
+
+    dataset.draw_sample(
+        779,
+        cmap_features=cm.viridis, cmap_targets=cm.coolwarm,
+        resolution=cfg.plotting.resolution,
+        xlim=cfg.plotting.xlim, ylim=cfg.plotting.ylim
+    )
+
     plt.show()
 
 
