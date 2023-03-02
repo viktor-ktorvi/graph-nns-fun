f1c33335c2214cee3781938b18347e0c90b7b5b1
viktor-ltorvi <todosijevicviktor998@gmail.com>
2023-03-02 19:02:43 +0100

    log relative loss for regression

--- train/gnn.py
+++ train/gnn.py
@@ -11,11 +11,11 @@ def train_gnn(model, optimizer, criterion, train_dataset, validation_dataset, cf
     progress_bar = tqdm(range(cfg.model.epochs))
     for epoch in progress_bar:
 
-        if epoch == 100:
-            debug_var = None
-
         cumulative_loss_train = 0
         accuracy_count_train = 0
+        cumulative_abs_error_train = 0
+        cumulative_abs_target_train = 0
+
         for batch in train_dataloader:
             batch = batch.to(cfg.training.device)
 
@@ -26,12 +26,16 @@ def train_gnn(model, optimizer, criterion, train_dataset, validation_dataset, cf
             cumulative_loss_train += loss.item()
             prediction = output > 0
             accuracy_count_train += (prediction == batch.y).sum().item()
+            cumulative_abs_error_train += torch.abs(output - batch.y).sum().item()
+            cumulative_abs_target_train += torch.abs(batch.y).sum().item()
 
             loss.backward()
             optimizer.step()
 
         cumulative_loss_val = 0
         accuracy_count_val = 0
+        cumulative_abs_error_val = 0
+        cumulative_abs_target_val = 0
 
         with torch.no_grad():
             for batch in validation_dataloader:
@@ -42,11 +46,16 @@ def train_gnn(model, optimizer, criterion, train_dataset, validation_dataset, cf
                 cumulative_loss_val += loss.item()
                 prediction = output > 0
                 accuracy_count_val += (prediction == batch.y).sum().item()
+                cumulative_abs_error_val += torch.abs(output - batch.y).sum().item()
+                cumulative_abs_target_val += torch.abs(batch.y).sum().item()
 
         if epoch % 1 == 0:
-            progress_bar.set_description("GNN training: training loss = {:2.5f}; validation loss = {:2.5f}; train accuracy = {:2.2f}; validation accuracy = {:2.2f}".format(
-                cumulative_loss_train / len(train_dataset),
-                cumulative_loss_val / len(validation_dataset),
-                accuracy_count_train / len(train_dataset) / cfg.topology.nodes,
-                accuracy_count_val / len(validation_dataset) / cfg.topology.nodes,
-            ))
+            progress_bar.set_description(
+                "GNN training: training loss = {:2.5f}; validation loss = {:2.5f}; train accuracy = {:2.2f}; validation accuracy = {:2.2f}; train relative error = {:2.5f}; validation relative error = {:2.5f}".format(
+                    cumulative_loss_train / len(train_dataset),
+                    cumulative_loss_val / len(validation_dataset),
+                    accuracy_count_train / len(train_dataset) / cfg.topology.nodes,
+                    accuracy_count_val / len(validation_dataset) / cfg.topology.nodes,
+                    cumulative_abs_error_train / cumulative_abs_target_train,
+                    cumulative_abs_error_val / cumulative_abs_target_val
+                ))
--- train/mlp.py
+++ train/mlp.py
@@ -17,6 +17,8 @@ def train_mlp(model, optimizer, criterion, train_dataset, validation_dataset, cf
     for epoch in progress_bar:
         cumulative_loss_train = 0
         accuracy_count_train = 0
+        cumulative_abs_error_train = 0
+        cumulative_abs_target_train = 0
 
         for batch in train_dataloader:
             x = batch[0].to(cfg.training.device)
@@ -29,12 +31,16 @@ def train_mlp(model, optimizer, criterion, train_dataset, validation_dataset, cf
             cumulative_loss_train += loss.item()
             prediction = output > 0
             accuracy_count_train += (prediction == y).sum().item()
+            cumulative_abs_error_train += torch.abs(output - y).sum().item()
+            cumulative_abs_target_train += torch.abs(y).sum().item()
 
             loss.backward()
             optimizer.step()
 
         cumulative_loss_val = 0
         accuracy_count_val = 0
+        cumulative_abs_error_val = 0
+        cumulative_abs_target_val = 0
 
         with torch.no_grad():
             for batch in validation_dataloader:
@@ -46,13 +52,18 @@ def train_mlp(model, optimizer, criterion, train_dataset, validation_dataset, cf
                 cumulative_loss_val += loss.item()
                 prediction = output > 0
                 accuracy_count_val += (prediction == y).sum().item()
+                cumulative_abs_error_val += torch.abs(output - y).sum().item()
+                cumulative_abs_target_val += torch.abs(y).sum().item()
 
         if epoch % 1 == 0:
-            progress_bar.set_description("MLP training: training loss = {:2.5f}; validation loss = {:2.5f}; train accuracy = {:2.2f}; validation accuracy = {:2.2f}".format(
-                cumulative_loss_train / len(train_dataset),
-                cumulative_loss_val / len(validation_dataset),
-                accuracy_count_train / len(train_dataset) / cfg.topology.nodes,
-                accuracy_count_val / len(validation_dataset) / cfg.topology.nodes,
-            ))
+            progress_bar.set_description(
+                "MLP training: training loss = {:2.5f}; validation loss = {:2.5f}; train accuracy = {:2.2f}; validation accuracy = {:2.2f}; train relative error = {:2.5f}; validation relative error = {:2.5f}".format(
+                    cumulative_loss_train / len(train_dataset),
+                    cumulative_loss_val / len(validation_dataset),
+                    accuracy_count_train / len(train_dataset) / cfg.topology.nodes,
+                    accuracy_count_val / len(validation_dataset) / cfg.topology.nodes,
+                    cumulative_abs_error_train / cumulative_abs_target_train,
+                    cumulative_abs_error_val / cumulative_abs_target_val
+                ))
 
-        # TODO log
+            # TODO log
